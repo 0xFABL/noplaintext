3890c7a16946c4d5e49632541488cb8f1511ac8f
Jacky <gokurohono@gmail.com>
2025-11-30 16:49:31 +1100

    WIP: progress

--- src/content.js
+++ src/content.js
@@ -54,37 +54,37 @@ function collectAllPageText() {
   // 4. Check if the text matches expected symbol regex.
   const matching_text = allText.filter((val) => {
     if (val.match(EXPECTED_SYMBOL_REGEX)) {
-      return True
+      return true
     }
-    return False
+    return false
   });
 
   return matching_text
 }
 
-const COMMAND_SCRAPE = "scrape";
 
 
 
 // connection
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.command === "inputFromPopup") {
-    // const inputData = request.data;
-    console.log('Content Script received message from Popup:', inputData);
+    const inputData = request.data;
+    // console.log('Content Script received message from Popup:', inputData);
 
     // // Optional: Display confirmation on the current page
     alert(`Content Script received: "${inputData}". Now sending to Service Worker.`);
 
-    // const inputData = collectAllPageText();
+    const scrapedData = collectAllPageText();
+    console.log(scrapedData);
 
-    // 2. Send the received data to the Service Worker
-    chrome.runtime.sendMessage({
-      command: "inputFromContent",
-      data: inputData
-    }, (response) => {
-      // 3. (Optional) Get a response back from the Service Worker
-      console.log('Response from Service Worker:', response);
-    });
+    // // 2. Send the received data to the Service Worker
+    // chrome.runtime.sendMessage({
+    //   command: "inputFromContent",
+    //   data: inputData
+    // }, (response) => {
+    //   // 3. (Optional) Get a response back from the Service Worker
+    //   console.log('Response from Service Worker:', response);
+    // });
 
     // Acknowledge receipt of the message
     sendResponse({ status: "Content script processed popup message" });
